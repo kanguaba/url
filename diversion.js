503bc9a1b0f24592ee9a079ffd46d82fee23a369
kanguaba <aishangni222@outlook.com>
2024-05-17 20:44:05 +0800

    Create diversion.js

---
+++ diversion.js
@@ -0,0 +1,1 @@
+["https:\/\/cg01.shop\/home\/ziYAQhgoKS.html","https:\/\/665312.xyz\/home\/RqfxFKegVH.html","https:\/\/cg03.shop\/home\/jTQUaBIatJ.html","https:\/\/cg04.shop\/home\/svQLCgnhPX.html"]
